eab5c7b0d296b374d3a51e1f0d41cf3275884e37
biggerstar <1458048116@qq.com>
2023-12-09 00:52:37 +0800

    typings: 更改类型提示

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,3 @@
-
 /**
  * 深度合并
  * @param target 目标对象
@@ -6,4 +5,4 @@
  * @param options
  * @param options.safe  是否是安全合并, 如果指定安全合并的话如果目标对象没有的键不会被合并
  * */
-export function deepmerge<T extends Record<any, any>>(target: T, source: Record<any, any>, options:{safe?: boolean}): T;
+export function deepmerge<T extends Record<string | symbol | number, any>>(target: T, source: Record<any, any>, options?: { safe?: boolean }): T;
